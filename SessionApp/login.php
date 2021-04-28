68ab6ed7efa54e9a1dd747efcaa1f708e9945e3f
jukmali <54353202+jukmali@users.noreply.github.com>
2021-04-28 08:43:03 +0300

    Update login.php
    user -> customer

--- SessionApp/login.php
+++ SessionApp/login.php
@@ -20,7 +20,7 @@
 
       if (!empty($username) && !empty($password)) {
         // Fetch the userid, username, password, and city from the database
-        $query = "SELECT userid, username, city FROM user WHERE username = '$username' AND password = SHA('$password')";
+        $query = "SELECT userid, username, city FROM customer WHERE username = '$username' AND password = SHA('$password')";
         $data = mysqli_query($dbc, $query);
 
         if (mysqli_num_rows($data) == 1) {
